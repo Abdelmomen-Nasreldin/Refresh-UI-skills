10b15a3a333ac1a4454f1d9cdec2b1aa5ef5e50e
Abdelmomen-Nasreldin <92486538+Abdelmomen-Nasreldin@users.noreply.github.com>
2022-03-16 22:54:51 +0200

    deals sections done

---
+++ js/owlCarousel.js
@@ -0,0 +1,84 @@
+$(function() {
+
+	if ( $('.owl-carousel-deals').length > 0 ) {
+        $('.owl-carousel-deals').owlCarousel({
+          //  center:false,
+            
+            // smartSpeed: 500,
+            // autoplay: true,
+            items:4,
+            nav: true,
+            dots: true,
+            loop: true,
+            pauseOnHover: true,
+            responsive:{
+                0:{
+                    margin: 20,
+                    nav: false,
+                    items: 1
+                },
+                768:{
+                    margin: 20,
+                    nav: true,
+                    items: 2
+                },
+                1000:{  
+                    margin: 20,
+                  nav: true,
+                  items: 3
+                },
+                1280:{
+                    margin: 20,
+                    nav: true,
+                    items: 4
+                },
+                1500:{
+                    margin: 20,
+                    nav: true,
+                    items: 5
+                },
+            }
+        });            
+    }
+	if ( $('.owl-carousel-blog ').length > 0 ) {
+        $('.owl-carousel-blog ').owlCarousel({
+          //  center:false,
+            
+            // smartSpeed: 500,
+            // autoplay: true,
+            items:3,
+            nav: true,
+            dots: true,
+            loop: true,
+            pauseOnHover: true,
+            responsive:{
+                0:{
+                    margin: 20,
+                    nav: false,
+                    items: 1
+                },
+                768:{
+                    margin: 20,
+                    nav: true,
+                    items: 2
+                },
+                1000:{  
+                    margin: 20,
+                  nav: true,
+                  items: 3
+                },
+                1280:{
+                    margin: 20,
+                    nav: true,
+                    items: 3
+                },
+                1500:{
+                    margin: 20,
+                    nav: true,
+                    items: 3
+                },
+            }
+        });            
+    }
+
+})
